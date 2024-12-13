363f309a4b8ccb63caa29134d55fdd494dab0f62
Gayoon1234 <s3843584@student.rmit.edu.au>
2024-12-13 21:24:31 +1100

    day4p1 refactor

--- 4/code/day4.rb
+++ 4/code/day4.rb
@@ -3,20 +3,21 @@ require_relative '../../lib/common'
 input = parse_input_as_array(get_input_path(4))
 
 ### PART 1 ###
-
-# Regex each line of XMAS or SAMX
-# This picks up all of the horizontal words
-h_total =  input.sum do |h_line|
-    h_line.scan(/XMAS/).length +  h_line.scan(/SAMX/).length
+def count_xmas_smax(arr)
+    arr.sum do |d_line|
+        d_line.scan(/XMAS/).length +  d_line.scan(/SAMX/).length
+    end
 end
+
+# Regex match each line of XMAS or SAMX
+# This picks up all of the horizontal words
+h_total = count_xmas_smax(input)
 
 # By transposing the array we can use the same approach for the
 # vertical words.
 transposed_input = input.map(&:chars).transpose.map(&:join)
 
-v_total = transposed_input.sum do |v_line|
-    v_line.scan(/XMAS/).length + v_line.scan(/SAMX/).length
-end
+v_total = count_xmas_smax(transposed_input)
 
 # First we pad the array so that all of the positive-diagonal words are
 # realigned to be vertical. Then transpose it so that all of the diagonal
@@ -29,9 +30,7 @@ increment = (
     .transpose
     .map(&:join)
 
-pg_total =  increment.sum do |d_line|
-    d_line.scan(/XMAS/).length +  d_line.scan(/SAMX/).length
-end
+pg_total = count_xmas_smax(increment)
 
 # Same as above but for negative-diagonal words
 decrement = (
@@ -42,8 +41,6 @@ decrement = (
     .transpose
     .map(&:join)
 
-ng_total =  decrement.sum do |d_line|
-    d_line.scan(/XMAS/).length +  d_line.scan(/SAMX/).length
-end
+ng_total =  count_xmas_smax(decrement)
 
 pp v_total + h_total + pg_total + ng_total
