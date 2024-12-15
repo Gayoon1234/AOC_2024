2995c70dbde5d33e3f98c277c6ba63e53bbd9243
Gayoon1234 <s3843584@student.rmit.edu.au>
2024-12-15 14:33:46 +1100

    Add TestRunner

---
+++ TestRunner.rb
@@ -0,0 +1,18 @@
+root_dir = File.dirname(__FILE__)
+
+(1..25).each do |day|
+  day_dir = File.join(root_dir, day.to_s, 'code')
+  day_file = File.join(day_dir, "day#{day}.rb")
+
+  if File.exist?(day_file)  
+    puts "***   DAY #{day}  ***" 
+    output = `ruby #{day_file}`
+    outputs = output.split("\n")  
+
+    puts "Part 1: " << outputs[0]
+    puts "Part 2: " << outputs[1] if outputs.length == 2
+    
+  else
+    puts "Skipped #{day}"
+  end
+end
